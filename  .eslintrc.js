df71e3f0d805a515086c2b1fd3ae6f9a289f77f2
Macbook <stephjaiye@gmail.com>
2024-11-08 14:42:04 +0100

    file added

---
+++  .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ["next", "next/core-web-vitals"], // Preset Next.js settings if not already configured
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  };
+  

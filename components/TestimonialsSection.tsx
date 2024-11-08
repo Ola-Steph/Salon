0b97730bfb52c3c98d1d90dbf7d6107130074cc2
Macbook <stephjaiye@gmail.com>
2024-11-08 14:23:14 +0100

    deleted approach

--- components/TestimonialsSection.tsx
+++ components/TestimonialsSection.tsx
@@ -20,7 +20,7 @@ const TestimonialsSection: React.FC = () => {
             <div className="text-yellow-500 text-lg mb-4">★★★★★</div>
             {/* eslint-disable-next-line react/no-unescaped-entities */}
             <p className="text-gray-700 mb-4">
-              "My experience was wonderful. Carol is a great hair stylist, understood what was needed to treat my hair and the style I requested. The salon environment is very clean and professional, and I felt very comfortable. As a customer, I was seen at the time my appointment was scheduled. I highly recommend 'Just Because'."
+              My experience was wonderful. Carol is a great hair stylist, understood what was needed to treat my hair and the style I requested. The salon environment is very clean and professional, and I felt very comfortable. As a customer, I was seen at the time my appointment was scheduled. I highly recommend Esther Hair Salon.
             </p>
             <p className="font-semibold text-gray-600">- Norma Elliott</p>
           </div>
@@ -30,7 +30,7 @@ const TestimonialsSection: React.FC = () => {
             <div className="text-yellow-500 text-lg mb-4">★★★★★</div>
             {/* eslint-disable-next-line react/no-unescaped-entities */}
             <p className="text-gray-700 mb-4">
-              "I had a silk press today. I absolutely love my hair. Definitely going back in the future. Great Experience."
+              I had a silk press today. I absolutely love my hair. Definitely going back in the future. Great Experience.
             </p>
             <p className="font-semibold text-gray-600">- Andrea Harris</p>
           </div>
@@ -40,7 +40,7 @@ const TestimonialsSection: React.FC = () => {
             <div className="text-yellow-500 text-lg mb-4">★★★★★</div>
             {/* eslint-disable-next-line react/no-unescaped-entities */}
             <p className="text-gray-700 mb-4">
-              "I went twice to Just Because. It is worth going to the salon coming from Queens. It is also easy for me because I work in the area. Already my natural hair feels healthy because of their natural products. I'm very satisfied with the natural products. I used the 8oil and now the spray. I look forward to my next appointment."
+              I went twice to Just Because. It is worth going to the salon coming from Queens. It is also easy for me because I work in the area. Already my natural hair feels healthy because of their natural products. I'm very satisfied with the natural products. I used the 8oil and now the spray. I look forward to my next appointment.
             </p>
             <p className="font-semibold text-gray-600">- Cs</p>
           </div>

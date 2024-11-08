2d55386c26cabb4b48a0505bb278e0e372bf6357
Macbook <stephjaiye@gmail.com>
2024-11-08 14:16:33 +0100

    tried again

--- components/TestimonialsSection.tsx
+++ components/TestimonialsSection.tsx
@@ -1,4 +1,5 @@
 // TestimonialsSection.tsx
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 
 const TestimonialsSection: React.FC = () => {
@@ -18,33 +19,27 @@ const TestimonialsSection: React.FC = () => {
           {/* Column 1 */}
           <div className="p-4">
             <div className="text-yellow-500 text-lg mb-4">★★★★★</div>
-            {/* eslint-disable react/no-unescaped-entities */}
             <p className="text-gray-700 mb-4">
               "My experience was wonderful. Carol is a great hair stylist, understood what was needed to treat my hair and the style I requested. The salon environment is very clean and professional, and I felt very comfortable. As a customer, I was seen at the time my appointment was scheduled. I highly recommend 'Just Because'."
             </p>
-            {/* eslint-enable react/no-unescaped-entities */}
             <p className="font-semibold text-gray-600">- Norma Elliott</p>
           </div>
 
           {/* Column 2 */}
           <div className="p-4">
             <div className="text-yellow-500 text-lg mb-4">★★★★★</div>
-            {/* eslint-disable react/no-unescaped-entities */}
             <p className="text-gray-700 mb-4">
               "I had a silk press today. I absolutely love my hair. Definitely going back in the future. Great Experience."
             </p>
-            {/* eslint-enable react/no-unescaped-entities */}
             <p className="font-semibold text-gray-600">- Andrea Harris</p>
           </div>
 
           {/* Column 3 */}
           <div className="p-4">
             <div className="text-yellow-500 text-lg mb-4">★★★★★</div>
-            {/* eslint-disable react/no-unescaped-entities */}
             <p className="text-gray-700 mb-4">
               "I went twice to Just Because. It is worth going to the salon coming from Queens. It is also easy for me because I work in the area. Already my natural hair feels healthy because of their natural products. I'm very satisfied with the natural products. I used the 8oil and now the spray. I look forward to my next appointment."
             </p>
-            {/* eslint-enable react/no-unescaped-entities */}
             <p className="font-semibold text-gray-600">- Cs</p>
           </div>
         </div>
@@ -61,3 +56,4 @@ const TestimonialsSection: React.FC = () => {
 };
 
 export default TestimonialsSection;
+/* eslint-enable react/no-unescaped-entities */
